6538c70c725f0378c6ee2c4946d29e0c7c99a539
Alim Uddin Asif <66546728+asifalim@users.noreply.github.com>
2022-11-20 13:45:35 +0600

    Update 1021 - Painful Bases.cpp

--- lightoj/1021 - Painful Bases.cpp
+++ lightoj/1021 - Painful Bases.cpp
@@ -26,22 +26,14 @@ int get(char ch)
 int64_t run(int mask,int rem)
 {
     if(__builtin_popcount(mask)==n)return rem==0;
-
     if(~dp[mask][rem])return dp[mask][rem];
     int64_t ans=0;
-    bool ck[19];// it indicate after storing some elements the possible answer for remaining elements
-    mem(ck,false);  // e.g: 12345-->Let current elements 125 so we should search for remaining by positioned them after current elements
-    for(int i=0;i<n;i++)
-    {
-        if(!((1LL<<i)&mask) and !ck[get(s[i])]){
-        ans+=run(mask|(1LL<<i),(get(s[i])+rem*base)%m);
-        ck[get(s[i])]=true;}
-    }
+    for(int i=0;i<n;i++)if(!((1LL<<i)&mask))ans+=run(mask|(1LL<<i),(get(s[i])+rem*base)%m);
     return dp[mask][rem]=ans;
 }
 signed main()
 {
-    //fast;
+    fast;
     w(t)
     {
         cin>>base>>m;
